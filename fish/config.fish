03dcd473359f557054950ee54215f61ad1740586
Neuman F <61904986+neumanf@users.noreply.github.com>
2022-08-07 18:55:27 -0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -32,3 +32,17 @@ function gcp
 	git add . && git commit -m $argv && git push
 end
 
+function project
+  set project_dir ~/Projects
+  set session_uuid project
+
+  tmux new -d -s "$session_uuid"
+  tmux send -t"$session_uuid:" "cd $project_dir/app1 && npm run dev" Enter
+  tmux splitw -t "$session_uuid:" -dh
+  tmux send -t"$session_uuid:.1" "cd $project_dir/app2 && npm run dev" Enter
+  tmux splitw -t "$session_uuid:.1" -dh
+  tmux send -t"$session_uuid:.2" "cd $project_dir/app3 && npm run dev" Enter
+  tmux splitw -t "$session_uuid:.2" -dh
+  tmux send -t"$session_uuid:.3" "cd $project_dir/app4 && yarn start" Enter
+  tmux attach -t "$session_uuid"
+end
